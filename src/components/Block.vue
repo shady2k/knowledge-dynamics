8fe5dabb16fc3b7dc621e2bd8c2341bf98e23d72
shady2k <shady2k@gmail.com>
2021-02-28 01:05:44 +0300

    Working with block component

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,21 +1,25 @@
 <template>
     <div class="flex flex-row my-1">
-        <div class="flex justify-center items-center">
+        <div class="flex mt-2">
             <span class="bullet"></span>
         </div>
-        <div class="ml-1 text-black w-full text-xl bg-gray-200" @click="clickDiv">
-            <span v-if="!isEdit" class=""
-                @click="clickSpan"
-            >
-                {{ text }}
+        <div
+            class="ml-1 mr-6 text-black w-full text-lg flex"
+            @click="clickDiv"
+        >
+            <span v-if="!isEdit" class="text-justify" @click="clickSpan">
+                {{ data }}
             </span>
             <textarea
                 v-if="isEdit"
                 @blur="blurElement"
-                v-model="text"
+                v-model="data"
                 autocapitalize="none"
                 rows="1"
                 ref="editor"
+                role="textbox"
+                @keydown.esc="$event.target.blur()"
+                v-autogrow
                 class="break-words overflow-hidden bg-transparent resize-none outline-none w-full bg-white"
             >
             </textarea>
@@ -25,33 +29,54 @@
 
 <script>
 export default {
+    props: {
+        blockId: Number
+    },
+    mounted() {
+    },
+    computed: {
+        data: {
+            get: function() {
+                let block = null;
+                block = this.$store.state.element.blocks.find((item) => {
+                    return item.id === this.blockId;
+                });
+                if (!block) {
+                    block.data = "Нажмите здесь, чтобы начать редактирования";
+                }
+                return block.data;
+            },
+            set: function(data) {
+                this.$store.commit('changeBlock', { id: this.blockId, data });
+            }
+        }
+    },
     data() {
         return {
             isEdit: false,
-            text: "Нажмите здесь, чтобы начать редактирование",
         };
     },
     methods: {
         clickDiv: function(e) {
-            if(!this.isEdit) {
+            if (!this.isEdit) {
                 this.clickSpan(e);
             }
         },
         clickSpan: function(e) {
             const range = window.getSelection().getRangeAt(0);
-			let target = e.target;
-			
-            while (target.tagName.toLowerCase() !== 'div') {
-				target = target.parentElement;
-				if (!target) return;
-			}
+            let target = e.target;
+
+            while (target.tagName.toLowerCase() !== "div") {
+                target = target.parentElement;
+                if (!target) return;
+            }
 
             const selectionLength = range.endOffset - range.startOffset;
             range.setStart(target, 0);
             let end = range.toString().length;
-            if(end >= 1) end--;
+            if (end >= 1) end--;
             let start = end - selectionLength;
-            
+
             this.isEdit = !this.isEdit;
             this.$nextTick(() => {
                 this.$refs.editor.focus();
@@ -60,8 +85,8 @@ export default {
         },
         blurElement: function() {
             this.isEdit = !this.isEdit;
-        }
-    }
+        },
+    },
 };
 </script>
 
